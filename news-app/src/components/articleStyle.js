1e569bf2d44c9e55acf9bbbf1548162b5895a1c7
Siddhant <sidhantranjan31@gmail.com>
2021-10-10 18:01:13 +0530

    removed unwanted code

--- news-app/src/components/articleStyle.js
+++ news-app/src/components/articleStyle.js
@@ -4,18 +4,5 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles({
   activeCard: {
     borderTop: '10px solid rgb(23, 126, 129)',
-    "$news-card__details-wrapper": {
-      maxHeight: "20rem",
-      opacity: 1
-    },
-    "$news-card__text-wrapper": {
-      backgroundColor: "rgb(23, 126, 129)"
-    },
-    "$news-card__title": {
-      color: yellow
-    },
-    "$news-card__image": {
-      transform: "scale(1.2)",
-    }
   },
 });
